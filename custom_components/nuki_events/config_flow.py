1c54a2eb4ef7179ffe38e39a905dca2948b9acaa
titiviking <73351833+titiviking@users.noreply.github.com>
2026-01-27 16:46:27 +0100

    Add HACS metadata

---
+++ custom_components/nuki_events/config_flow.py
@@ -0,0 +1,16 @@
+"""Config flow for Nuki Events."""
+from __future__ import annotations
+
+from homeassistant.helpers import config_entry_oauth2_flow
+
+from .const import DEFAULT_OAUTH_NAME, DOMAIN
+
+
+class NukiOAuth2FlowHandler(
+    config_entry_oauth2_flow.OAuth2FlowHandler, domain=DOMAIN
+):
+    """Handle OAuth2 configuration flow for Nuki."""
+
+    async def async_oauth_create_entry(self, data):
+        """Create an entry for the flow."""
+        return self.async_create_entry(title=DEFAULT_OAUTH_NAME, data=data)
